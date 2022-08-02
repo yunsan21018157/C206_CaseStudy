e3403728a328ebcffa84be7526582286185599ee
21018947 <21018947@rp.edu.sg>
2022-08-02 15:48:59 +0800

    Initial Commit

--- C206_CaseStudy/src/C206_CaseStudy.java
+++
@@ -1,9 +0,0 @@
-
-public class C206_CaseStudy {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
---
+++ C206_CaseStudy/src/TuitionManagement.java
@@ -0,0 +1,47 @@
+
+public class TuitionManagement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+	}
+	
+	public static void role() {
+		System.out.println("1 - Student");
+		System.out.println("2 - Manager");
+		System.out.println("3 - Admin");
+		System.out.println("------------------------------");
+	}
+	
+	public static void menuStudent() {
+		System.out.println("1 - Register Student Account");
+		System.out.println("2 - View Student Account");
+		System.out.println("3 - Delete Student Account");
+		System.out.println("4 - Register for Tuition");
+		System.out.println("5 - View Registered Tuitions");
+		System.out.println("6 - Delete Registered Tuitions");
+		System.out.println("------------------------------");
+	}
+	
+	public static void menuManager() {
+		System.out.println("1 - Add Tuition Timetable");
+		System.out.println("2 - View Tuition Timetable");
+		System.out.println("3 - Delete Tuition Timetable");
+		System.out.println("4 - Add Teacher");
+		System.out.println("5 - View Teacher");
+		System.out.println("6 - Delete Teacher");
+		System.out.println("------------------------------");
+	}
+	
+	public static void menuAdmin() {
+		System.out.println("1 - Register Student Account");
+		System.out.println("2 - Add Tuition Information");
+		System.out.println("3 - View Tuition Information");
+		System.out.println("4 - Delete Tuition Information");
+		System.out.println("------------------------------");
+	}
+	
+}
