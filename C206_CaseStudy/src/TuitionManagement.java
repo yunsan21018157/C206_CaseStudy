c0de6d7d9d4a297ca169519f6919e441cf52260a
21018947 <21018947@rp.edu.sg>
2022-08-04 14:23:33 +0800

    Main Code commit

--- C206_CaseStudy/src/TuitionManagement.java
+++ C206_CaseStudy/src/TuitionManagement.java
@@ -14,6 +14,67 @@ public class TuitionManagement {
 		
 		tuitionTimetableList.add(new TuitionTimetable(1, 180.0, "01/01/2022", "01/03/2022", "Online"));
 		tuitionTimetableList.add(new TuitionTimetable(2, 180.0, "01/04/2022", "01/07/2022", "F2F"));
+		
+		//code
+		role();
+		int role = Helper.readInt("Enter role index: ");
+		System.out.println("------------------------------");
+		
+		int studentChoice = 0;
+		int managerChoice = 0;
+		int adminChoice = 0;
+		
+		while (role != 4) {
+			if (role == 1) {
+				menuStudent();
+				studentChoice = Helper.readInt("Enter your choice: ");
+				System.out.println("------------------------------");
+				while (studentChoice != 6) {
+					
+				}
+			} else if (role == 2) {
+				menuManager();
+				managerChoice = Helper.readInt("Enter your choice: ");
+				System.out.println("------------------------------");
+				while (managerChoice != 7) {
+					if (managerChoice == 1) {
+						addTuitionTimetable(tuitionTimetableList);
+						
+						menuManager();
+						managerChoice = Helper.readInt("Enter your choice: ");
+						System.out.println("------------------------------");
+					} else if (managerChoice == 2) {
+						viewTuitionTimetable(tuitionTimetableList);
+						menuManager();
+						managerChoice = Helper.readInt("Enter your choice: ");
+						System.out.println("------------------------------");
+					} else if (managerChoice == 3) {
+						deleteTuitionTimetable(tuitionTimetableList);
+						menuManager();
+						managerChoice = Helper.readInt("Enter your choice: ");
+						System.out.println("------------------------------");
+					} else if (managerChoice == 4) {
+						
+					} else if (managerChoice == 5) {
+						
+					} else if (managerChoice == 6) {
+						
+					} else {
+						managerChoice = Helper.readInt("Invalid choice, Enter your choice again: ");
+					}
+				} System.out.println("Account Logged out.");
+				System.out.println("------------------------------");
+				role();
+				role = Helper.readInt("Enter role index: ");
+			} else if (role == 3) {
+				menuAdmin();
+				adminChoice = Helper.readInt("Enter your choice: ");
+			} else if (role == 4) {
+				System.out.println("Program Ended.");
+			} else {
+				role = Helper.readInt("Invalid Index, enter role index again: ");
+			}
+		}
 	}
 	
 	public static void role() {
@@ -30,7 +91,7 @@ public class TuitionManagement {
 		System.out.println("3 - Register for Tuition");
 		System.out.println("4 - View Tuition Registration");
 		System.out.println("5 - Delete Tuition registration");
-		System.out.println("6 - Quit program");
+		System.out.println("6 - Log Out");
 		System.out.println("------------------------------");
 	}
 	
@@ -41,7 +102,7 @@ public class TuitionManagement {
 		System.out.println("4 - Add Teacher");
 		System.out.println("5 - View Teacher");
 		System.out.println("6 - Delete Teacher");
-		System.out.println("7 - Quit program");
+		System.out.println("7 - Log Out");
 		System.out.println("------------------------------");
 	}
 	
@@ -51,7 +112,7 @@ public class TuitionManagement {
 		System.out.println("3 - Add Tuition Information");
 		System.out.println("4 - View Tuition Information");
 		System.out.println("5 - Delete Tuition Information");
-		System.out.println("6 - Quit program");
+		System.out.println("6 - Log Out");
 		System.out.println("------------------------------");
 	}
 	
@@ -87,9 +148,7 @@ public class TuitionManagement {
 	public static void addTuitionTimeTable(ArrayList<TuitionTimetable> tuitionList, Tuition tuition) {
 			
 		tuitionList.add(tuition);
-			
 	}
-}
 	
 	//================================= Option 5 View tuition registration (menuStudent()) YIXUN ==================================
 	
@@ -97,21 +156,50 @@ public class TuitionManagement {
 	//================================= Option 6 Delete tuition registration (menuStudent()) YIXUN ==================================
 	
 	
-	//================================= Option 1 Add tuition timetable (menuManager()) JOSEPH =================================
-	public static ArrayList<managerTimetable> addTimetable() {
-		setTtId(Helper.readInt"");
-		private double price;
-		private String start_date;
-		private String end_date;
-		private String mode;
-	}
+    //================================= Option 1 Add tuition timetable (menuManager()) JOSEPH =================================
+	public static void addTuitionTimetable(ArrayList<TuitionTimetable> tuitionTimetableList) {
 		
+		int ttID = Helper.readInt("Enter tuition timetable ID: ");
+		double price = Helper.readDouble("Enter tuition price: ");
+		String start_date = Helper.readString("Enter tuition starting date: ");
+		String end_date = Helper.readString("Enter tuition ending date: ");
+		String mode = Helper.readString("Enter tuition mode (Online/Mixed/FTF): ");
+		
+		tuitionTimetableList.add(new TuitionTimetable(ttID, price, start_date, end_date, mode));
+		
+		System.out.println("Timetable has been added.");
+		System.out.println("------------------------------");
+	}
 	
 	//================================= Option 2 View tuition timetable (menuManager()) JOSEPH =================================
-	
+	public static void viewTuitionTimetable(ArrayList<TuitionTimetable> tuitionTimetableList) {
+			
+		for (int i = 0; i < tuitionTimetableList.size(); i++) {
+			System.out.println("Tuition Timetable ID: " + tuitionTimetableList.get(i).getTtId());
+			System.out.println("Tuition Timetable Price: " + tuitionTimetableList.get(i).getPrice());
+			System.out.println("Tuition Timetable Starting Date: " + tuitionTimetableList.get(i).getStart_date());
+			System.out.println("Tuition Timetable Ending Date: " + tuitionTimetableList.get(i).getEnd_date());
+			System.out.println("Tuition Timetable Mode: " + tuitionTimetableList.get(i).getMode());
+			System.out.println("--------------------------------------------------");
+		}
+	}
 	
 	//================================= Option 3 Delete tuition timetable (menuManager()) JOSEPH =================================
-	
+	public static void deleteTuitionTimetable(ArrayList<TuitionTimetable> tuitionTimetableList) {
+		
+		int del_ttID = Helper.readInt("Enter the tuition timetable ID you want to delete: ");
+		
+		for (int i = 0; i < tuitionTimetableList.size(); i++) {
+			int ttID = tuitionTimetableList.get(i).getTtId();
+			if (ttID == del_ttID) {
+				tuitionTimetableList.remove(i);
+				System.out.println("Timetable removed.");
+				System.out.println("------------------------------");
+			} else {
+				continue;
+			}
+		}
+	}
 	
 	//================================= Option 4 Add teacher (menuManager()) JAYDEN =================================
 	
